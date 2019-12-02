db20655321a6d53661b2b43c04854c539b44a080
Jake Wija <jakewija@icloud.com[core]>
2019-12-02 15:26:29 -0800

    FINISH getElementsByClassName function using both an inner recursive function and with Array.prototype.concat() method. Previously, I had only solved it with the inner loop method that appended to a list that would return. The updated approach is more in the spirit of a truly recursive function.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,38 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+
+// WITH NESTED RECURSIVE FUNCTION
+// var getElementsByClassName = function(className) {
+//   var elements = [];
+
+//   var searchNodes = function(node) {
+//     if (node.classList.contains(className)) {
+//       elements.push(node);
+//     }
+//     Array.from(node.children).forEach(searchNodes);
+//   };
+//   searchNodes(document.body);
+//   return elements;
+// };
+
+
+// WIHTOUT NESTED RECURSIVE FUNCTION, USING ARRAY.PROTOTYPE.CONCAT()
+var getElementsByClassName = function( className, node ){
+
+  var results = [];
+  //if node argument is undefined at function call, make the node the body of the document
+  node = node || document.body;
+  // take the node and test if it 1. has a classList, and 2. contains the className.
+  if (node.classList && node.classList.contains(className)) {
+    results.push(node);
+  }
+  // repeat recursively if node has children, stop if not
+  var children = node.childNodes;
+  if (children) {
+    children.forEach(function(child){
+      results = results.concat(getElementsByClassName(className, child));
+    })
+  }
+  return results;
 };
